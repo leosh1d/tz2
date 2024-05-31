8d197e5c4bc8065b0e0a54723b837db3f04a9caa
leosh1d <leosh1dddd@gmail.com>
2024-06-01 00:42:30 +0300

    Add disabled test

--- src/test/java/NumberProcessorTest.java
+++ src/test/java/NumberProcessorTest.java
@@ -1,4 +1,7 @@
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
 import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,32 +10,58 @@ class NumberProcessorTest {
     @Test
     void testMinMax() {
         List<Integer> numbers = Arrays.asList(1, 4, 2, 3);
-        assertEquals(1, NumberProcessor._min(numbers));
-        assertEquals(4, NumberProcessor._max(numbers));
+        assertEquals(1, NumberProcessor.getMin(numbers));
+        assertEquals(4, NumberProcessor.getMax(numbers));
     }
 
     @Test
     void testSumMult() {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
-        assertEquals(10, NumberProcessor._sum(numbers));
-        assertEquals(24, NumberProcessor._mult(numbers));
+        assertEquals(10, NumberProcessor.getSum(numbers));
+        assertEquals(24, NumberProcessor.getMult(numbers));
+    }
+
+    List<Integer> generateListByCount(int count){
+        List<Integer> largeList = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            largeList.add(i);
+        }
+
+        return largeList;
     }
 
     @Test
     void testPerformance() {
         // Генерируем список с 5 млн. чисел
-        List<Integer> largeList = new ArrayList<>();
-        for (int i = 0; i < 5_000_000; i++) {
-            largeList.add(i);
-        }
+        List<Integer> largeList = generateListByCount(5_000_000);
 
         long startTime = System.currentTimeMillis();
 
         // Выполняем операции
-        NumberProcessor._min(largeList);
-        NumberProcessor._max(largeList);
-        NumberProcessor._sum(largeList);
-        NumberProcessor._mult(largeList);
+        NumberProcessor.getMin(largeList);
+        NumberProcessor.getMax(largeList);
+        NumberProcessor.getSum(largeList);
+        NumberProcessor.getMult(largeList);
+
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+
+        System.out.println("Время выполнения для 5 млн. чисел: " + duration + " мс");
+    }
+
+    @Disabled
+    @Test
+    void failedTst() {
+        // Генерируем список с 5 млн. чисел
+        List<Integer> largeList = generateListByCount((int) Math.pow(10, 10));
+
+        long startTime = System.currentTimeMillis();
+
+        // Выполняем операции
+        NumberProcessor.getMin(largeList);
+        NumberProcessor.getMax(largeList);
+        NumberProcessor.getSum(largeList);
+        NumberProcessor.getMult(largeList);
 
         long endTime = System.currentTimeMillis();
         long duration = endTime - startTime;
